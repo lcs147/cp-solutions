d9c511ea94b78e7e7d0218c93372ff8c04fbac63
Lucas Castro <lcs147@hotmail.com>
2023-09-23 02:27:44 -0400

    getting the codes

--- main.py
+++ main.py
@@ -1,28 +1,44 @@
+from html.parser import HTMLParser
 import json
+import bs4
 import requests
+from bs4 import BeautifulSoup
+import mechanicalsoup
+from urllib.request import urlopen
 
 
 def get_all_submissions(username):
     return json.loads(
-        requests.get(f"https://codeforces.com/api/user.status?handle=lcs147").text
-        # requests.get(f"https://codeforces.com/api/user.status?handle=lcs147&from=1&count=1").text
+        # requests.get(f"https://codeforces.com/api/user.status?handle=lcs147").text
+        requests.get(f"https://codeforces.com/api/user.status?handle=lcs147&from=1&count=1").text
     )["result"]
 
 
+def getCode(url):
+    response = requests.Session().get(url, headers={"User-Agent": "Mozilla/5.0"})
+    soup = BeautifulSoup(response.text, "html.parser")
+    code = soup.find(id="program-source-text").text
+    return code
+
+
 def main():
-    username = input("Enter codeforces username: ")
+    username = "lcs147"
     subs = get_all_submissions(username)
     accs = []
     for sub in subs:
         if sub["verdict"] == "OK":
+            print(sub)
             accs.append(
                 {
-                    "id": sub["id"],
-                    "link": f'https://codeforces.com/contest/{sub["problem"]["contestId"]}/submission/"{sub["id"]}',
+                    "sub_id": sub["id"],
+                    "link": f'https://codeforces.com/contest/{sub["problem"]["contestId"]}/submission/{sub["id"]}',
+                    "problem_id": f"{sub['problem']['contestId']}{sub['problem']['index']}",
                 }
             )
     print(len(accs), accs[0])
-    # save all submissions
+
+    for sub in accs:
+        sub["code"] = getCode(sub["link"])
 
 
 if __name__ == "__main__":
